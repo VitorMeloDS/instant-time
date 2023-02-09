039798f7436e3bc7e23ecb675897ca69afa6df81
Vitor Melo da Silva <vmsvitor20@gmail.com>
2023-02-08 21:38:12 -0300

    feat: add method for mask the date

--- index.js
+++ index.js
@@ -1,18 +1,55 @@
 /**
  * @param {Date} date Data que deseja formatar.
- * @returns {String} Retona uma string contendo a data com o formato especificado.
+ * @param {String} options mascara options 'dd/mm/yyyy' ou 'dd/mm/yyyy hh:mm:ss'.
+ * @returns {String} Returns a string containing the date with the specified format.
  */
-function DateFormat (date) {
-  return new Intl.DateTimeFormat('pt-BR', {
-    timeZone: 'America/Sao_Paulo',
-    day: '2-digit',
-    month: '2-digit',
-    year: '2-digit',
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric',
-    hour12: false
-  }).formatToParts(date).map(({type, value}) => value).join('');
+function DateFormat(date, options) {
+  return new Intl.DateTimeFormat('pt-BR', optionsDateTime(options)).formatToParts(date).map(({type, value}) => value).join('');
+}
+
+/**
+ * @param {String} options mascara options 'dd/mm/yyyy' ou 'dd/mm/yyyy hh:mm:ss'.
+ * @returns {Object} Returns object with mask date.
+ */
+function optionsDateTime(options) {
+  /**
+   * @type {Object} object that contains the date mask.
+   */
+  let maskDate;
+
+  switch (options?.toLowerCase()) {
+    case 'dd/mm/yyyy':
+      maskDate = {
+        timeZone: 'America/Sao_Paulo',
+        day: '2-digit',
+        month: '2-digit',
+        year: '2-digit',
+      };
+
+      break;
+    case 'dd/mm/yyyy hh:mm:ss':
+      maskDate = {
+        timeZone: 'America/Sao_Paulo',
+        day: '2-digit',
+        month: '2-digit',
+        year: '2-digit',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+        hour12: false
+      };
+
+      break;
+    default:
+      maskDate = {
+        timeZone: 'America/Sao_Paulo',
+        day: '2-digit',
+        month: '2-digit',
+        year: '2-digit',
+      };
+    }
+
+  return maskDate;
 }
 
 module.exports = {
